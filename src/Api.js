cce5254f93295678ee6f41df5dd7b62ac13dbc8b
manassorn <manassorn@gmail.com>
2021-04-13 21:11:40 +0700

    ff

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookie from './Cookie'
 
 const Api = axios.create({
-  baseURL: 'http://1hourtutor.com:3000/api',
+  baseURL: 'http://1hourtutor.com/api',
   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' }
 });
