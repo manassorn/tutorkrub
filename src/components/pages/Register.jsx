e03edae8ebbbed7de87ec5fe885a03bb3d83d57c
manassorn <manassorn@gmail.com>
2021-05-03 21:30:03 +0700

    c

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -32,7 +32,7 @@ class Register extends React.Component {
       <SimpleTitle title='ลงทะเบียน' />
        
       <div class="p-3">
-
+<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="true"></div>
         <button className="btn btn-primary btn-block">login with facebook</button>
         <button className="btn btn-primary btn-block">login with Line</button>
         
