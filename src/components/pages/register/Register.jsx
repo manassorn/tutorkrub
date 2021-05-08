744ba5dafcba24c0406605968fb132a4c822385e
manassorn <manassorn@gmail.com>
2021-05-08 20:28:47 +0700

    hh

--- src/components/pages/register/Register.jsx
+++ src/components/pages/register/Register.jsx
@@ -43,9 +43,9 @@ class Register extends React.Component {
     loginFb() {
       window.FB.login(function(response) {
         if (response.authResponse) {
-          console.log('Welcome!  Fetching your information.... ');
+          alert('Welcome!  Fetching your information.... ');
           window.FB.api('/me', function(response) {
-            console.log('Good to see you, ' + response.name + '.');
+            alert('Good to see you, ' + response.name + '.');
           });
         } else {
           console.log('User cancelled login or did not fully authorize.');
