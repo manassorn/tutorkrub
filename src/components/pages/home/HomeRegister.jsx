05a715230fc989be27aa48f81671205bc5fbc6e4
manassorn <manassorn@gmail.com>
2021-05-15 15:45:20 +0700

    dd

--- src/components/pages/home/HomeRegister.jsx
+++ src/components/pages/home/HomeRegister.jsx
@@ -21,6 +21,12 @@ class HomeFooter extends React.Component {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
       </div>
       
+      
+          <div className="text-center mb-3">
+              <span className="h6">4 สิ่ง</span>
+              <p className="h4">ที่จะได้รับ</p>
+          </div>
+      
         <div className="row" style={{background:'#0099ff'}}>
             <div className="col-md-4 offset-md-2">
                 <div className="col-5 col-md-12 mx-auto">
@@ -99,6 +105,11 @@ class HomeFooter extends React.Component {
       <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100" viewBox="0 0 1440 320" preserveAspectRatio="none"><path fill="#0099ff" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,229.3C384,235,480,277,576,250.7C672,224,768,128,864,117.3C960,107,1056,181,1152,202.7C1248,224,1344,192,1392,176L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
       </div>
       
+      
+          <div className="text-center mb-3">
+              <span className="h6">ผู้คน</span>
+              <p className="h4">ที่กล่าวชมเรา</p>
+          </div>
       
       
       <div className="row mx-0 px-md-5 py-4 bg-light">
