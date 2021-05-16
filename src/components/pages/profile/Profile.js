7055eba151b67fa19d674285b065229f04e12d71
manassorn <manassorn@gmail.com>
2021-05-16 13:48:12 +0700

    cc

--- src/components/pages/profile/Profile.js
+++ src/components/pages/profile/Profile.js
@@ -95,17 +95,14 @@ class Profile extends React.Component {
       <div class="course-card col-lg-4">
         <div class="card radius-10 border">
          <div className="card-header">คอร์ส</div>
-         <div class="card-body row">
-          <div className="col-md-10">
+         <div class="card-body">
             <h5 className="card-title">{course.title}</h5>
-            <span className="card-subtitle">{course.schoolLevel} . {course.subject}</span>
-            <p class="card-text">฿{course.price}</p>
+            <p class="card-text">{course.schoolLevel} . {course.subject}</p>
 
-          </div>
-          <div className="col-md-2">
-            <a href={`/course/edit/${course.id}`} class="btn btn-white btn-block stretched-link">แก้ไข</a>
+            <h6 className="card-subtitle">฿{course.price}</h6>
 
-          </div>
+            <a href={`/course/edit/${course.id}`} class="card-link stretched-link">แก้ไข</a>
+
 
          </div>
         </div>
