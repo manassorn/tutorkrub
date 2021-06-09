06baf5da91eba6b8aff7c20f1e12935d8428a886
manassorn <manassorn@gmail.com>
2021-06-09 20:44:52 +0700

    hh

--- src/components/common/CalendarPartOfDay.js
+++ src/components/common/CalendarPartOfDay.js
@@ -168,6 +168,7 @@ class CalendarPartOfDay extends React.Component {
       const h = d*24+p*6+i
       for(let j = this.state.layers.length - 1; j>=0; j--) {
         console.log('j', j)
+        console.log(this.state.layers[j])
         console.log('h', h)
         if (this.state.layers[j][h]) {
           return ''
