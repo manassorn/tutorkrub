050bb36fb097ba8f5cb2d73e1711210aae878342
manassorn <manassorn@gmail.com>
2021-06-09 16:18:07 +0700

    gg

--- src/components/common/CalendarPartOfDay.js
+++ src/components/common/CalendarPartOfDay.js
@@ -155,9 +155,6 @@ class CalendarPartOfDay extends React.Component {
     }
     
     getHrClassName(d,p,i) {
-      if(this.state.modalCheckboxes[i]) {
-        return 'green-glow'
-      }
       const hr = d*24+p*6+i
       if (this.state.layer4Hrs && this.state.layer4Hrs[hr]) {
         return 'red-glow'
