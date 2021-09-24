72ec291cab86274879956ac7b568994b6894dc9c
manassorn <manassorn@gmail.com>
2021-09-24 20:29:40 +0700

    gh

--- src/Auth.js
+++ src/Auth.js
@@ -14,6 +14,7 @@ const Auth ={
     return json.userId
   },
   checkLogin: () => {
+    return
     Api.get('/users/me').then(function(response) {
       return
       const user = response.data.data
