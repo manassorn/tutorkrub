be81c56faca192253cb6df3072f7ad91a7287d7c
manassorn <manassorn@gmail.com>
2021-09-25 21:21:25 +0700

    gh

--- src/components/pages/course/CourseCheckout.jsx
+++ src/components/pages/course/CourseCheckout.jsx
@@ -127,7 +127,8 @@ class CourseCheckout extends React.Component {
       </div>
       
       <div className="row border-top">
-      <div className="col">
+      <div className="col-sm-7">
+      <h6>ช่องทางชำระเงิน</h6>
       <div class="form-check p-3 px-4 border rounded">
 
 				<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked/>
