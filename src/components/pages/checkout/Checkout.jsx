9be473c867fa3445c9a66d48a829c9cdcbb9817b
manassorn <manassorn@gmail.com>
2021-09-27 20:42:15 +0700

    rg

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -150,9 +150,23 @@ class Checkout extends React.Component {
 
 
       <div className="p-3 fixed-bottom bg-white w-100 border-top">
-        <button className="btn btn-primary btn-block" >ชำระเงิน</button>
+        <button className="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModalCenter">ชำระเงิน</button>
       </div>
 
+      
+      <!-- Modal --> 
+      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true"> 
+      <div class="modal-dialog modal-dialog-centered" role="document"> 
+      <div class="modal-content"> 
+      <div class="modal-header border-0"> 
+      <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5> 
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> 
+      </div> 
+      <div class="modal-body"> ... </div> 
+      <div class="modal-footer" hidden="true"> <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save changes</button> </div> 
+      </div> 
+      </div> 
+      </div>
 
      </div>
 
