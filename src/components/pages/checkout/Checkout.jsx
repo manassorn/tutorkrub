db07cf6a19fd21bfa7dd6e11409e878f3059f611
manassorn <manassorn@gmail.com>
2021-09-27 20:43:15 +0700

    ghk

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -154,7 +154,6 @@ class Checkout extends React.Component {
       </div>
 
       
-      <!-- Modal --> 
       <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true"> 
       <div class="modal-dialog modal-dialog-centered" role="document"> 
       <div class="modal-content"> 
