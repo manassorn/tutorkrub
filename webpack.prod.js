4e5a31558b9d2772bfeedfc8b33033483353b772
manassorn <manassorn@gmail.com>
2021-10-09 10:58:06 +0700

    bgg

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
+const CopyPlugin = require("copy-webpack-plugin");
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [ new CopyPlugin({ patterns: [ { from: "assets", to: "dist/assets2" } ], options: { concurrency: 100, }, }), ],
 });
