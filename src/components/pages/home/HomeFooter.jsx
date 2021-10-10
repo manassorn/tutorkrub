1f85242b2584a6c583f6341f00799e21941f4606
manassorn <manassorn@gmail.com>
2021-10-10 20:42:56 +0700

    fjjd

--- src/components/pages/home/HomeFooter.jsx
+++ src/components/pages/home/HomeFooter.jsx
@@ -13,7 +13,9 @@ class HomeFooter extends React.Component {
       return <div className="p-3 border-top bg-light">
         <div className="d-flex bx-xl">
           <i className="bx bxl-facebook-circle" />
-          <i className="bx bxl-youtube-circle" />
+          <i className="lni lni-facebook-original" />
+          <i className="lni lni-youtube" />
+          <i className="lni lni-instagram-original" />
           <i className="bx bxl-twitter-circle" />
         
         </div>
