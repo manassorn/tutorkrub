506be763c72e15b5bfefe0848098d10352728639
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2021-10-12 11:11:12 +0700

    a

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Auth from "./Auth"
 import Wrapper from "./components/common/Wrapper"
 import Course from "./components/pages/course/Course"
 import Checkout from "./components/pages/checkout/Checkout"
+import CheckoutCreditCard from "./components/pages/checkout/CheckoutCreditCard"
 import CourseAdd from "./components/pages/course/CourseAdd"
 import CourseEdit from "./components/pages/course/CourseEdit"
 import Explore from "./components/pages/explore/Explore"
@@ -49,6 +50,7 @@ class App extends React.Component {
               <Route path="/appointment/list" component={AppointmentList} />
               <Route path="/appointment" component={Appointment} />
               <Route path="/checkout" component={Checkout} />
+              <Route path="/checkout/creditcard" component={CheckoutCreditCard} />
               <Route path="/course/add" component={CourseAdd} />
               <Route path="/course/edit" component={CourseEdit} />
               <Route path="/explore" component={Explore} />
