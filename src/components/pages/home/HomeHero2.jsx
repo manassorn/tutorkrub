a532575c1dc27bf974040b6f3bc213745f63b553
manassorn <manassorn@gmail.com>
2021-11-02 20:07:03 +0700

    fjf

--- src/components/pages/home/HomeHero2.jsx
+++ src/components/pages/home/HomeHero2.jsx
@@ -17,7 +17,17 @@ class HomeHero2 extends React.Component {
   render() {
     return <div className="xhero-bg-young-boy row mx-0" style={{background:'#fafafa'}}>
         <div className="col-md-6 hero-message">
-        <div class="jumbotron jumbotron-fluid"> <div class="container"> <h1 class="display-4">Fluid jumbotron</h1> <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> </div> </div>
+        <div class="jumbotron jumbotron-fluid"> 
+        <div class="container"> 
+        <h1 class="display-4">Fluid jumbotron</h1> 
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> 
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> 
+          <a class="btn btn-link btn-lg" href="#" role="button">Learn more</a> 
+        </p> 
+
+        </div> 
+        </div>
 
         <h1>สอน / เรียน แค่ 1 ชั่วโมง</h1>
         <div className="pl-2">
