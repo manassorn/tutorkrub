2e51c5858509070c4a14475ddbb387402bf84cca
manassorn <manassorn@gmail.com>
2021-11-04 20:46:19 +0700

    fhf

--- src/components/pages/home/HomeGettingStarted.js
+++ src/components/pages/home/HomeGettingStarted.js
@@ -14,95 +14,187 @@ class HomeGettingStarted extends React.Component {
 
     componentDidMount() {
       /*Api.get('/crud/course')
-        .then(response => 
+        .then(response =>
         {
           console.log('courses',response.data.data)
-        
+
         this.setState({ courses: response.data.data })
         }
         );
       */
     }
-    
+
     clickTab(tab) {
       this.setState({tab})
     }
 
     render() {
       return <div className="pt-5">
-          <div className="text-center mb-3">
-              <span className="h6">3 ขั้นตอนง่าย ๆ</span>
+          <div className="text-center">
+              <span className="h6">4 ขั้นตอน</span>
               <p className="h4">ง่ายๆ ในการ</p>
           </div>
-          
-          
-      <div className="row mx-0">
-          <div className="col-md-3 mb-4">
-            <div class="title-icon"> 
-            <i className="bx bx-id-card"></i>
-           </div> 
-           <div>
-              
-              <h4 class="font font-weight-bold">1.ลงทะเบียน</h4> 
-              <p className="">
-                กรอกชื่อ นามสกุล อีเมลหรือเบอร์โทรศัพท์ให้ครบถ้วน
-              </p>
-              <a href="/register" className="btn btn-block btn-outline-primary py-2">
+          <ul className="nav nav-border-bottom justify-content-center">
+              <li className="nav-item">
+                  <a className={`nav-link px-5 ${this.state.tab == 'tutor'? 'active' :''}`} href="#" onClick={e => this.clickTab('tutor')}>สอน</a>
+              </li>
+              <li className="nav-item">
+                  <a className={`nav-link px-5 ${this.state.tab == 'student'? 'active' :''}`} href="#" onClick={e => this.clickTab('student')}>เรียน</a>
+              </li>
+          </ul>
+
+          <div className="row mx-0">
+          <div className="offset-md-1 col-md-5 pt-4">
+
+
+
+          <div class="mt-2 media">
+           <div class="product-img">
+            <i className="bx bx-id-card" style={{fontSize:'30px'}}></i>
+           </div>
+
+
+
+
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font font-weight-bold">1. ลงทะเบียน</h6>
+            <p className="mb-1">
+            กรอกชื่อ นามสกุล อีเมลหรือเบอร์โทรศัพท์ให้ครบถ้วน
+            </p>
+            <a href="/register" className="btn btn-block btn-outline-primary py-2">              สมัครสมาชิก
+            </a>
+           </div>
+          </div>
+
+
+
+         <div class="mt-4 media">
+           <div class="product-img">
+            <i className="bx bx-calendar" style={{fontSize:'30px'}}></i>
+           </div>
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font-weight-bold">2. สร้างคอร์สเรียน</h6>
+            <p className="mb-1">
+  กรอกรายลัเอียดเยอะๆ เพื่อให้นักเรียนค้นพบได้ง่ายยิ่งขึ้น
+            </p>
+            <a href="/course/add" className="btn btn-block btn-outline-primary py-2">
+              สร้างคอร์ส
+            </a>
+           </div>
+          </div>
+
+
+         <div class="mt-4 media">
+           <div class="product-img">
+            <i className="bx bx-video" style={{fontSize:'30px'}}></i>
+           </div>
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font-weight-bold">3. เริ่มสอน</h6>
+            <p className="mb-1">
+ เตรียมตัวก่อนเวลานัดหมาย 10 นาที ตรวจสอบอินเตอร์และอุปกรณ์ว่าพร้อมใช้งาน
+            </p>
+           </div>
+          </div>
+
+
+
+
+         <div class="mt-4 media">
+           <div class="product-img">
+            <i className="bx bx-wallet" style={{fontSize:'30px'}}></i>
+           </div>
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font-weight-bold">4. รับเงินเมื่อสอนจบ</h6>
+            <p className="mb-1">
+ เมื่อคุณสอนจบ เงินจะถูกโอนเข้า "กระเป๋าตังค์" ของคุณ
+            </p>
+           </div>
+          </div>
+
+
+          </div>
+
+          <div className="col-md-5 border-left pt-4">
+
+          <div class="mt-2 media">
+           <div class="product-img">
+            <i className="bx bx-id-card" style={{fontSize:'30px'}}></i>
+           </div>
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font font-weight-bold">1. ลงทะเบียน</h6>
+            <p className="mb-1">
+            กรอกชื่อ นามสกุล อีเมลหรือเบอร์โทรศัพท์ให้ครบถ้วน
+            </p>
+            <a href="/course/add" className="btn btn-block btn-outline-primary py-2">
               สมัครสมาชิก
-              </a>
+            </a>
            </div>
           </div>
-          
-          
-          <div className="col-md-3 mb-4">
-            <div class="title-icon"> 
-            <i className="bx bx-calendar"></i>
-           </div> 
-           <div>
-              
-              <h4 class="font font-weight-bold">2.เลือกเวลาเรียนและชำระเงิน</h4> 
-              <p className="">
-                เลือกเวลาเรียนได้เอง เลือกเวลาเรียนที่สะดวก ไม่ว่าเสาร์อาทิตย์ หรือวันธรรมดา ไม่ว่าเช้าตรู่หรือเที่ยงคืน
-              </p>
-              <a href="/explore" className="btn btn-block btn-outline-primary py-2">
+
+
+         <div class="mt-4 media">
+           <div class="product-img">
+            <i className="bx bx-calendar" style={{fontSize:'30px'}}></i>
+           </div>
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font-weight-bold">2. เวลาเรียนและชำระเงิน</h6>
+            <p className="mb-1">
+  เลือกเวลาเรียนที่สะดวก
+            </p>
+            <a href="/course/add" className="btn btn-block btn-outline-primary py-2">
               ค้นหาวิชาเรียน
-              </a>
+            </a>
            </div>
           </div>
-          
-          <div className="col-md-3">
-            <div class="title-icon"> 
-            <i className="bx bx-video"></i>
-           </div> 
-           <div>
-              
-              <h4 class="font font-weight-bold">3.เริ่มเรียน</h4> 
-              <p className="">
-                เตรียมตัวก่อนเวลานัดหมาย 10 นาที ตรวจสอบอินเตอร์และอุปกรณ์ว่าพร้อมใช้งาน
-              </p>
+
+
+
+         <div class="mt-4 media">
+           <div class="product-img">
+            <i className="bx bx-video" style={{fontSize:'30px'}}></i>
+           </div>
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font-weight-bold">3. เริ่มเรียน</h6>
+            <p className="mb-1">
+ เตรียมตัวก่อนเวลานัดหมาย 10 นาที ตรวจสอบอินเตอร์และอุปกรณ์ว่าพร้อมใช้งาน
+            </p>
            </div>
           </div>
-          
-          
-          <div className="col-md-3">
-            <div class="title-icon"> 
-            <i className="bx bx-wallet"></i>
-           </div> 
-           <div>
-              
-              <h4 class="font font-weight-bold">4.เรียนจบ</h4> 
-              <p className="">
-                
+
+         <div class="mt-4 media">
+           <div class="product-img">
+            <i className="bx bx-wallet" style={{fontSize:'30px'}}></i>
+           </div>
+           <div class="media-body pl-3">
+            <h6 class="mb-0 font-weight-bold">4. เงินถูกโอนไปยังติวเตอร์</h6>
+            <p className="mb-1">
  เมื่อคุณเรียนจบ เงินจะถูกโอนเข้า "กระเป๋าตังค์" ของติวเตอร์
-              </p>
+            </p>
            </div>
           </div>
-          
+
+
+
+
+
+
           </div>
-          
+
+
+
+
+
+
+          </div>
+
+
+
+
+
+
       </div>
 
     }
 }
 
-export default HomeGettingStarted
+export default HomeGettingStarted;
