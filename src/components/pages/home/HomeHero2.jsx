59e2016c80844c7f872dcc95a3f76f3a9f732bf1
manassorn <manassorn@gmail.com>
2021-11-05 20:54:32 +0700

    fgh

--- src/components/pages/home/HomeHero2.jsx
+++ src/components/pages/home/HomeHero2.jsx
@@ -25,7 +25,7 @@ class HomeHero2 extends React.Component {
         ทดลองเรียน 1 ชั่วโมงก่อนได้
         </p> 
         <p class="leadx">
-          <a class="btn btn-primary font-weight-bold p-3" href="#" role="button">ทดลองเรียน 1 ชั่วโมง</a> 
+          <a class="btn btn-primary font-weight-bold px-4" href="#" role="button">ทดลองเรียน 1 ชั่วโมง</a> 
         </p> 
 
 
