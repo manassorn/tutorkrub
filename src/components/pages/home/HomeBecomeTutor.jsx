51c86bb8ebfbdc47bcfe54a116f4f501923cec4f
manassorn <manassorn@gmail.com>
2021-11-05 19:43:19 +0700

    fjf

--- src/components/pages/home/HomeBecomeTutor.jsx
+++ src/components/pages/home/HomeBecomeTutor.jsx
@@ -13,12 +13,12 @@ class HomeBecomeTutor extends React.Component {
       return <div className="p-3 my-5" id="home-contactus" style={{background:'#0099ff'}}>
           <div className="offset-md-2 col-md-8">
           <div className="row">
-              <div className="p-5 col">
+              <div className="p-5 col-8">
                   <span className="h6">สนใจเป็นติวเตอร์ ?</span>
                   <p className="h4">มาร่วมเป็นส่วนหนึ่งกับเรา สมัครได้เลย</p>
                   <a href="/register" className="btn btn-danger">สมัครเป็นติวเตอร์</a>
               </div>
-              <div className="col">
+              <div className="col-4 text-right">
                   <img src="/assets/images/home/become-tutor-point.png" width="150"/>
               </div>
             </div>
