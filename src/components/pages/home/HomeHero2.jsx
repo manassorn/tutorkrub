33d8e6f3aa8e5a2821b252dcdc5b1380550a904d
manassorn <manassorn@gmail.com>
2021-11-05 20:32:00 +0700

    jhh

--- src/components/pages/home/HomeHero2.jsx
+++ src/components/pages/home/HomeHero2.jsx
@@ -26,7 +26,7 @@ class HomeHero2 extends React.Component {
         ทดลองเรียน 1 ชั่วโมงก่อนได้
         </p> 
         <p class="leadx">
-          <a class="btn btn-primary" href="#" role="button">ทดลองเรียน 1 ชั่วโมง</a> 
+          <a class="btn btn-primary font-weight-bold" href="#" role="button">ทดลองเรียน 1 ชั่วโมง</a> 
           <a class="btn btn-link" href="#" role="button">อยากเป็นติวเตอร์</a> 
         </p> 
 
