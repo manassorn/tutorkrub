93dbd2e51f6c009324235ae6768fe68e2dfb0f7f
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2021-11-05 09:19:34 +0700

    a

--- src/components/pages/home/HomeBecomeTutor.jsx
+++ src/components/pages/home/HomeBecomeTutor.jsx
@@ -11,12 +11,12 @@ class HomeBecomeTutor extends React.Component {
     
     render() {
       return <div className="p-3 my-5" id="home-contactus">
-          <div className="offset-md-2 col-md-8" style={{background:'#0099ff'}}>
+          <div className="offset-md-2 col-md-8 p-5" style={{background:'#0099ff'}}>
               <span className="h6">สนใจเป็นติวเตอร์ ?</span>
               <p className="h4">มาร่วมเป็นส่วนหนึ่งกับเรา สมัครได้เลย</p>
               <a href="/register" className="btn btn-danger">สมัครเป็นติวเตอร์</a>
               <div style={{position:'absolute',bottom:0,right:'50px'}}>
-                  <img src="/assets/images/home/become-tutor-point.png" width="100"/>
+                  <img src="/assets/images/home/become-tutor-point.png" width="150"/>
               </div>
           </div>
 
