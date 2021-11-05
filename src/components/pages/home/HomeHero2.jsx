de2616e46374acac11f3bce06892ebd47b4b3391
manassorn <manassorn@gmail.com>
2021-11-05 20:29:40 +0700

    hh

--- src/components/pages/home/HomeHero2.jsx
+++ src/components/pages/home/HomeHero2.jsx
@@ -16,7 +16,7 @@ class HomeHero2 extends React.Component {
 
   render() {
     return <div className="xhero-bg-young-boy row mx-0" style={{background:'#fafafa'}}>
-        <div className="offset-md-1 col-md-5">
+        <div className="offset-md-1 col-md-6">
         <div class="jumbotron jumbotron-fluid pb-0" style={{background:'#fafafa'}}> 
         <div class="container"> 
         <h1 class="display-4">
@@ -35,7 +35,7 @@ class HomeHero2 extends React.Component {
 
       </div>
       
-      <div className="col-md-6" style={{paddingTop:'0px'}}>
+      <div className="col-md-5" style={{paddingTop:'0px'}}>
         <img src="/assets/images/home/skype-call.jpg" width="100%"/>
       </div>
      </div>
