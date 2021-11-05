ef913d349f8f25e071d9807316e2a55b28e4ee76
manassorn <manassorn@gmail.com>
2021-11-05 20:08:44 +0700

    djd

--- src/components/pages/home/HomeBecomeTutor.jsx
+++ src/components/pages/home/HomeBecomeTutor.jsx
@@ -14,8 +14,8 @@ class HomeBecomeTutor extends React.Component {
           <div className="offset-md-2 col-md-8">
           <div className="row">
               <div className="py-5 col-md-8">
-                  <span className="h6">สนใจเป็นติวเตอร์ ?</span>
-                  <p className="h4">มาร่วมเป็นส่วนหนึ่งกับเรา สมัครได้เลย</p>
+                  <span className="h4">สนใจเป็นติวเตอร์ </span>
+                  <p className="lead">มาร่วมเป็นส่วนหนึ่งกับเรา </p>
                   <a href="/register" className="btn btn-danger">สมัครเป็นติวเตอร์</a>
               </div>
               <div className="col-md-4 text-right">
