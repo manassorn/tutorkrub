7af9022a9fcbd2dd0b1f800e1b7bd23e4397068a
manassorn <manassorn@gmail.com>
2021-11-08 20:51:09 +0700

    djd

--- src/components/pages/home/HomeBecomeTutor.jsx
+++ src/components/pages/home/HomeBecomeTutor.jsx
@@ -11,8 +11,8 @@ class HomeBecomeTutor extends React.Component {
     
     render() {
       return <div className="" id="" style={{background:'#002f5f'}}>
-          <div className="text-center">
-            <h2 className="text-white">สนใจเป็นติวเตอร์  สมัครได้เลย</h2>
+          <div className="text-center p-3">
+            <h2 className="text-white p-3">สนใจเป็นติวเตอร์  สมัครได้เลย</h2>
             <a href="/register" className="btn btn-outline-light">สมัครเป็นติวเตอร์</a>
           </div>
 
