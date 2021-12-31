74d511ea195f840ae80f349efeee3b42ec809970
manassorn <manassorn@gmail.com>
2021-12-31 20:38:48 +0700

    xjx

--- src/components/pages/appointment/AppointmentChat.js
+++ src/components/pages/appointment/AppointmentChat.js
@@ -104,7 +104,7 @@ class Appointment extends React.Component {
         
         <h3>ข้อความ</h3>
         
-        <div className="text-center text-muted my-4">
+        <div className="text-center text-muted my-5">
         
         <i className="bx bx-chat" style={{fontSize: '50px'}}></i>
         <br/>
