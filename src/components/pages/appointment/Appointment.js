b41c1f18b1d37b00bcbec0e3a6cf41a63a0f13be
manassorn <manassorn@gmail.com>
2022-01-01 21:21:42 +0700

    gg

--- src/components/pages/appointment/Appointment.js
+++ src/components/pages/appointment/Appointment.js
@@ -37,7 +37,7 @@ class Appointment extends React.Component {
       
        <SimpleTitle title='รายละเอียดนัดหมาย' />
        
-      <div className="row my-4">
+      <div className="row my-4 border-bottom pb-4">
         <div className="col-4 col-md-3 offset-md-1 border-right">
           <div className="text-center">
             <h5 className="card-title">มกราคม<br/>12</h5>
