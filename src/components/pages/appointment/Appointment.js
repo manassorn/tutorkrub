ab78d3deab8abe3b9ea10b84c638f9afbc5eb4c7
manassorn <manassorn@gmail.com>
2022-01-06 22:13:48 +0700

    gf

--- src/components/pages/appointment/Appointment.js
+++ src/components/pages/appointment/Appointment.js
@@ -38,7 +38,7 @@ class Appointment extends React.Component {
       <div className="row my-4 border-bottom pb-4">
       <div className="offset-md-1 col-md-10">
       <div className="row">
-        <div className="col-4 border-right">
+        <div className="col-12 bg-light p-3">
           <div className="text-center">
             <h5 className="card-title">มกราคม<br/>12</h5>
             <h6 className="card-subtitle">
@@ -51,28 +51,13 @@ class Appointment extends React.Component {
             </div>
           </div>
         </div>
-        <div className="col-8">
-          <h5 className="card-title">Course Title</h5>
-          <p className="card-text"><span className="text-muted">โดย Manassorn Vanichdilokkul</span></p>
-
-        </div>
+ 
         
 
         
-        <div className="col-6 mt-3">        <button className="btn btn-primary btn-block radius-10">ตกลง</button></div>
-        <div className="col-6 mt-3">        <button className="btn btn-danger btn-block radius-10">ปฏิเสธ</button></div>
-        
-      <div className="col-12 mt-3">
 
-        <div className="alertx alert-secondaryx text-center">
-        <span className="text-primary">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/> </svg>
-        </span>
-        <span style={{verticalAlign:'bottom'}}>
-          โปรดตอบรับภายใน 12 ม.ค. 13:00
-        </span>
-        </div>
-        </div>
+        
+
         
         
         <div className="col-12">
@@ -97,8 +82,21 @@ class Appointment extends React.Component {
           <img src="" width="32" height="32" class="rounded-circle shadow" alt=""/>
         &nbsp;&nbsp;
         <span>Manassorn Vanichdilokkul</span>
+        </div>
         
+        <div className="col-6 mt-3">        <button className="btn btn-primary btn-block radius-10">ตกลง</button></div>
+        <div className="col-6 mt-3">        <button className="btn btn-danger btn-block radius-10">ปฏิเสธ</button></div>
+        
+      <div className="col-12 mt-3">
 
+        <div className="alertx alert-secondaryx text-center">
+        <span className="text-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/> </svg>
+        </span>
+        <span style={{verticalAlign:'bottom'}}>
+          โปรดตอบรับภายใน 12 ม.ค. 13:00
+        </span>
+        </div>
         </div>
         
       </div>
