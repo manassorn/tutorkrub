10cc92338ef0ae9443816d3a26a0a9c0847dabb1
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-01-07 10:27:28 +0700

    a

--- src/components/pages/appointment/AppointmentList.js
+++ src/components/pages/appointment/AppointmentList.js
@@ -53,21 +53,13 @@ class AppointmentList extends React.Component {
 
   render() {
     return <div class="container" style={{maxWidth:'720px'}}> 
-      <div class="mb-3 mt-3">
-       <ul class="nav nav-tabs"> 
-        <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#Experience"><span class="p-tab-name1">นัดเรียน</span> <span class="badge badge-pill badge-danger">3</span></a> </li> 
-        <li class="nav-item"> <a class="nav-link" id="profile-tab" data-toggle="tab" href="#Biography"><span class="p-tab-name1">นัดสอน</span> <span class="badge badge-pill badge-danger">3</span></a> </li> 
-       </ul> 
-      </div> 
+
       
       
-      
-      <div className="mb-3">
+      <div className="my-4">
         <div className="btn-group d-flex d-sm-none" role="group" aria-label="Basic example">
         {this.icons.map((icon, i) => (
           <button type="button" className="btn btn-white font-13 w-25" style={{position:'relative'}}>
-          
-            <i className={"bx bx-md my-0 " + icon}></i><br/>
           {this.labels[i]}
           <span className="badge badge-pill badge-danger" style={{position:'absolute',top:'10px',right:'10px'}}>3</span>
           </button>
@@ -75,13 +67,11 @@ class AppointmentList extends React.Component {
         ))}
 
         </div>
-        <div className="btn-group d-none d-sm-block" role="group" aria-label="Basic example">
+        <div className="btn-group d-flex" role="group" aria-label="Basic example">
         {this.icons.map((icon, i) => (
           
-          <button type="button" className="btn btn-white">
-          
-            <i className={"bx " + icon}></i>&nbsp;
-          {this.labels[i]} &nbsp;
+          <button type="button" className="btn btn-white w-100">
+            {this.labels[i]}
           <span className="badge badge-pill badge-danger">3</span>
           </button>
         ))}
