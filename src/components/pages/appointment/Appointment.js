de54a9ee8562b3011255967c17d9b7eeb15da5db
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-01-07 09:28:10 +0700

    a

--- src/components/pages/appointment/Appointment.js
+++ src/components/pages/appointment/Appointment.js
@@ -35,8 +35,8 @@ class Appointment extends React.Component {
         return <div class="container pt-3 border-top" style={{maxWidth:'720px'}}>
       
       
-      <div className="row border-bottom pb-4">
-      <div className="offset-md-1 col-md-10">
+      <div className="row">
+      <div className="offset-md-1 col-md-10 border-bottom pb-4">
       <div className="row">
         <div className="col-12 bg-light p-3 mb-3">
           <div className="text-center">
@@ -90,10 +90,16 @@ class Appointment extends React.Component {
       <div className="col-12 mt-3">
 
         <div className="alertx alert-secondaryx text-center">
-        <span className="text-primary">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16"> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/> </svg>
+        <span style={{color:'#F2B504', verticalAlign:'text-bottom'}}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+             className="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+          <path
+            d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
+          <path
+            d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
+        </svg>
         </span>
-        <span style={{verticalAlign:'bottom'}}>
+        <span>
           โปรดตอบรับภายใน 12 ม.ค. 13:00
         </span>
         </div>
