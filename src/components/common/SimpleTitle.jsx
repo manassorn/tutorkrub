7c5089bf077c59b53fe98b529dfa14cd7ed12ddd
manassorn <manassorn@gmail.com>
2022-01-07 20:37:41 +0700

    dj

--- src/components/common/SimpleTitle.jsx
+++ src/components/common/SimpleTitle.jsx
@@ -11,16 +11,16 @@ class SimpleTitle extends React.Component {
 
     render() {
 
-        return <div class="p-3 text-center border-bottom" style={{position:'relative'}}> 
+        return <div className="p-3 text-center border-bottom" style={{position:'relative'}}> 
           {this.props.backBtn && (
             <a href="javascript:history.back()" style={{position:'absolute',left:'15px',top:'8px'}}>
-              <i class="bx bx-chevron-left text-dark" style={{fontSize:'36px'}}></i>
+              <i className="bx bx-chevron-left text-dark" style={{fontSize:'36px'}}></i>
             </a>
           )}
 
 
 
-          <h3 class="mb-0">{this.props.title}</h3> 
+          <h3 className="mb-0">{this.props.title}</h3> 
         </div>
     }
 };
