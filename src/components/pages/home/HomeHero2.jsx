aae467717e348de338f6b1b72a44ea3b4d678820
manassorn <manassorn@gmail.com>
2022-01-09 22:05:01 +0700

    gd

--- src/components/pages/home/HomeHero2.jsx
+++ src/components/pages/home/HomeHero2.jsx
@@ -18,9 +18,12 @@ class HomeHero2 extends React.Component {
     return <div className="row mx-0 py-5 align-items-center" style={{background:'#fafafa'}}>
         <div className="offset-md-1 col-md-6 px-5 text-center text-md-left">
         
+        <h1>
+          <img src="/assets/images/logo1.png" alt="" height="54" width="54"/>
+          1HourTutor
+
+        </h1>
         <h2>
-          <img src="/assets/images/logo1.png" alt="" height="48" width="48"/>
-          1HourTutor<br/>
 
         แพลตฟอร์มเชื่อมโยงระหว่างติวเตอร์และนักเรียน
         </h2> 
@@ -34,8 +37,17 @@ class HomeHero2 extends React.Component {
 
       </div>
       
-      <div className="col-md-5" style={{paddingTop:'0px'}}>
+      <div className="col-md-5 pt-0">
         <img src="/assets/images/home/skype-call3.jpg" width="100%"/>
+      </div>
+      
+      <div className="d-none d-md-block">
+        <h4>ค้นหาติวเตอร์ที่สอนได้ "ตรงใจ"</h4>
+        <ul>
+          <li>เรียนออนไลน์</li>
+          <li>เลือกเวลาเรียนเองได้</li>
+          <li>ชำระเงินเป็นครั้ง ครั้งละ 1-2 ชั่วโมง</li>
+        </ul>
       </div>
      </div>
 
