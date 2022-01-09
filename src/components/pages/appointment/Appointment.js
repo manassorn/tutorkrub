0fd9a83060522b345e263a0ffaed3dd444c5fe6f
manassorn <manassorn@gmail.com>
2022-01-09 14:26:06 +0700

    ff

--- src/components/pages/appointment/Appointment.js
+++ src/components/pages/appointment/Appointment.js
@@ -39,8 +39,9 @@ class Appointment extends React.Component {
       <div className="offset-md-1 col-md-10 border-bottom pb-4">
       <div className="row">
         <div className="col-12 bg-light p-3 mb-3">
-        <i class='bx bx-dots-horizontal-rounded float-right font-24'></i>
           <div className="text-center">
+                  <i class='bx bx-dots-horizontal-rounded float-right font-24'></i>
+
             <h5 className="card-title">มกราคม<br/>12</h5>
             <h6 className="card-subtitle">
               10:00
