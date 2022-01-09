b7004f76b6473101e886bc7b118e30ee459700cc
manassorn <manassorn@gmail.com>
2022-01-09 18:17:11 +0700

    fh

--- src/components/pages/home/HomeFooter.jsx
+++ src/components/pages/home/HomeFooter.jsx
@@ -10,11 +10,11 @@ class HomeFooter extends React.Component {
     }
     
     render() {
-      return <div className="p-3 border-top bg-light" style={{background:'#262626',color:'#fdfdfd'}}>
+      return <div className="p-3 border-top" style={{background:'#262626',color:'#fdfdfd'}}>
       <div className="row">
         
         <div className="text-center col-md-5 offset-md-1 text-md-left">
-            <img src="/assets/images/logo1.png" className="logo-icon" alt="" height="48"/>
+            <img src="/assets/images/logo1.png" className="logo-icon" alt="" height="48" width="48"/>
         </div>
         <div className="text-center col-md-5 text-md-right">
           วันอาวติวเตอร์
