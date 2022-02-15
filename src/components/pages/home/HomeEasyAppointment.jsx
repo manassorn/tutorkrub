f8b4eddcb23f40b300217d06a0fa32371ec889b2
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-02-15 15:06:14 +0700

    asdf

--- src/components/pages/home/HomeEasyAppointment.jsx
+++ src/components/pages/home/HomeEasyAppointment.jsx
@@ -26,22 +26,19 @@ class HomeEasyAppointment extends React.Component {
 
           <div className="row">
             <div className="col-md-10 offset-md-1">
-            <div className="d-flex flex-column flex-md-row justify-content-around">
-            
-              <div className="border m-1" style={{width:'200px'}}>
-               ddd
-              
-              </div>
-                        
-              <div className="border m-1" style={{width:'200px'}}>
-               ddd
-              
-              </div>
-              
-              <div className="border m-1" style={{width:'200px'}}>
-               ddd
-              
-              </div>
+            <div className="row">
+
+              {[0,1,2,3,4].map(i => (
+                <div className="col-md-3 px-4" style={{width:'200px'}}>
+                  <div style={{boxShadow:'rgba(0, 0, 0, 0.16) 0px 1px 4px;'}} className="text-center radius-10">
+                    <img src="https://i.imgur.com/BDmdbeI.png" style={{width: '60%'}} /><br/>
+                      <h2>asdf</h2>
+                      <p className="px-3 pb-3">dfsdfsdfsd asd asdf as fasd fasd fasd fasd fads fas fa sfas df asasdf.
+                        asf</p>
+                  </div>
+                </div>
+              ))}
+
             
             </div>
             
