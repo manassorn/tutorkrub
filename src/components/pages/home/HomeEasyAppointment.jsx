5a476c58c2e1b9d20f3a08d28ffcb5e20a2d0e2d
manassorn <manassorn@gmail.com>
2022-02-27 14:06:25 +0700

    gg

--- src/components/pages/home/HomeEasyAppointment.jsx
+++ src/components/pages/home/HomeEasyAppointment.jsx
@@ -50,10 +50,10 @@ class HomeEasyAppointment extends React.Component {
 
             <div className="col-md-3">
             <div className="row">
-                <div className="col-3 align-self-center">
+                <div className="col-2 col-md-3 align-self-center">
                  <img src="http://v.fastcdn.co/u/c787f3ba/25900801-0-arrow.png" width="100%" />
                 </div>
-                <div className="col-9">
+                <div className="col-10 col-md-9">
                   <div className="text-center radius-10 box-shadow py-3">
                       <h4 className="text-weight-bold mb-0">{d.title}</h4>
                   </div>
