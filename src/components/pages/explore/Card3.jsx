92eae8f7241cf2d4eaa8494d3b14a36fede05e49
manassorn <manassorn@gmail.com>
2022-03-10 20:26:04 +0700

    cc

---
+++ src/components/pages/explore/Card3.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Api from '../../../Api'
+import Constant from '../../../Constant'
+import SimpleTitle from '../../common/SimpleTitle'
+
+class Card3 extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {}
+    }
+  }
+
+  render() {
+    return <div class="col-6 col-md-3">
+            <div class="card" style={{'box-shadow':'none'}}>
+                <div className="bg-light" style={{paddingTop:'100%',position:'relative'}}>
+                <img class="card-img-top rounded rounded-lg" src={course.tutor.avatarUrl} alt="" style={{position:'absolute',top:0}}/>
+                </div>
+                <div class="card-body p-0 mt-2">
+                    <p class="card-text">
+                        <a href={`/course/${course.id}`} className="stretched-link text-dark h6">
+                        {course.title}</a><br/>
+                                              <span class="badge badge-pill badge-secondary">{course.schoolLevel}</span>
+                                 &nbsp;             <span class="badge badge-pill badge-secondary">{course.subject}</span>
+
+                        <div className="mt-2 font-weight-bold h6">฿{course.price}/ชั่วโมง</div>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+  }
+}
+export default Card3
--- src/components/pages/explore/Explore.jsx
+++ src/components/pages/explore/Explore.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Api from '../../../Api'
 import Constant from '../../../Constant'
 import SimpleTitle from '../../common/SimpleTitle'
+import Card1 from './Card1'
+import Card2 from './Card2'
+import Card3 from './Card3'
 
 class Explore extends React.Component {
 
@@ -73,24 +76,7 @@ class Explore extends React.Component {
       <div id="course-cards" class="row">
       
       {this.state.courses.map(course => (
-<div class="col-6 col-md-3">
-            <div class="card" style={{'box-shadow':'none'}}>
-                <div className="bg-light" style={{paddingTop:'100%',position:'relative'}}>
-                <img class="card-img-top rounded rounded-lg" src={course.tutor.avatarUrl} alt="" style={{position:'absolute',top:0}}/>
-                </div>
-                <div class="card-body p-0 mt-2">
-                    <p class="card-text">
-                        <a href={`/course/${course.id}`} className="stretched-link text-dark h6">
-                        {course.title}</a><br/>
-                                              <span class="badge badge-pill badge-secondary">{course.schoolLevel}</span>
-                                 &nbsp;             <span class="badge badge-pill badge-secondary">{course.subject}</span>
-
-                        <div className="mt-2 font-weight-bold h6">฿{course.price}/ชั่วโมง</div>
-                    </p>
-                </div>
-            </div>
-        </div>
-        
+        <Card1 course={course} />
         
       ))}
         
