4eddefa0a7b83bc2af26861b979b691c0985a12a
manassorn <manassorn@gmail.com>
2022-03-11 20:43:27 +0700

    c

--- src/components/pages/explore/Card3.jsx
+++ src/components/pages/explore/Card3.jsx
@@ -23,9 +23,9 @@ class Card3 extends React.Component {
           <img className="mr-3 rounded-circle" src={course.tutor.avatarUrl} alt="" width="80"/>
         </div>
         <div className="flex-grow-1">
-          <h5 className="font-weight-bold mt-2">{course.tutor.name}</h5>
+          <h5 className="font-weight-bold mt-2 mb-1">{course.tutor.name}</h5>
           
-          <p className="text-secondary mb-0">ติวเตอร์</p>
+          <p className="text-secondary mb-3">ติวเตอร์</p>
           <div>
             <i className="lni lni-graduation bx-xs mr-1 text-primary" />
             <span>{course.subject}</span>
@@ -43,19 +43,7 @@ class Card3 extends React.Component {
         </div>
         <a href="#" style={{width:'16px'}}><img src="https://i.imgur.com/d6xHzmg.png" width="16" /></a>
       </div>
-      <div className="field mt-2">
-        <label>วิชา</label>
-      </div>
-      <div className="d-flex mt-2">
-        <div className="w-50 field">
-          <label className="">ชั้นเรียน</label>
-          <span>{course.schoolLevel || 'ม.1'}</span>
-        </div>
-        <div className="w-50 field">
-          <label className="">ราคาต่อชั่วโมง</label>
-          <span>฿{course.price} / ชั่วโมง</span>
-        </div>
-      </div>
+      
 
     </div>
     </div>
