b55f5caa645e0b335c46b5a726d036e2d9d92ef9
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-03-12 13:35:52 +0700

    aa

--- src/components/pages/profile/ProfileTutor.jsx
+++ src/components/pages/profile/ProfileTutor.jsx
@@ -28,14 +28,18 @@ class ProfileTutor extends React.Component {
     render() {
       
       return <ProfileLayout>
-        <ProfileGroup name="ข้อมูลส่วนบุคคล">
-          <ProfileField label="สอน" value="คณิตศาสตร์, อังกฤษ"></ProfileField>
+        <ProfileGroup name="ข้อมูลการสอน">
+          <ProfileField label="สอนวิชา" value="คณิตศาสตร์, อังกฤษ"></ProfileField>
           <ProfileField label="ชั้นเรียน" value="ม.1 - ม.6"></ProfileField>
           <ProfileField label="ราคาต่อชั่วโมง" value="฿100 / ชั่วโมง"></ProfileField>
         </ProfileGroup>
         <ProfileGroup name="การศึกษา">
           <ProfileField label="ปริญญาตรี" value="จุฬาลงกณ์มหาวิทยาลัย GPA 3.10"></ProfileField>
           <a href="#">+ เพิ่มการศึกษา</a>
+        </ProfileGroup>
+        <ProfileGroup name="ช่องทางติดต่อ">
+          <ProfileField label="Line" value="manassorn"></ProfileField>
+          <a href="#">+ เพิ่มช่องทางติดต่อ</a>
         </ProfileGroup>
         <ProfileGroup name="รายละเอียดอื่นๆ">
           <ul>
