14e76148778fc8ad1c692a2b1234482fe63b5736
manassorn <manassorn@gmail.com>
2022-03-21 20:21:31 +0700

    yy

--- src/components/pages/profile/ProfileTutorCongrats.jsx
+++ src/components/pages/profile/ProfileTutorCongrats.jsx
@@ -49,7 +49,7 @@ class ProfileTutorcongrats extends React.Component {
                 </div>
                 <div className="position-relative overflow-hidden" style={{height:'300px'}}>
                   <div className="my-4 text-center congrats-step congrats-step1" ref={ele => {this.step1 = ele}}>
-                  <div class="progress" style={{height:'2px'}}>
+                  <div className="my-4 progress" style={{height:'2px'}}>
   <div class="progress-bar" role="progressbar" style={{width:'33%'}} aria-valuenow="33" aria-valuemin="0" aria-valuemax="100"></div>
 </div>
                     <p>คุณสอนวิชาอะไร (เลือกได้หลายวิชา)</p>
