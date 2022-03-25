88a1b5be7fbe9ba4440a8ee4bc77d0e83a718959
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-03-25 20:29:46 +0700

    aa

--- src/components/pages/profile/ProfileTutorCongrats.jsx
+++ src/components/pages/profile/ProfileTutorCongrats.jsx
@@ -55,6 +55,7 @@ class ProfileTutorcongrats extends React.Component {
     render() {
       
       return <div>
+        <canvas id="canvas"></canvas>
         <div className="modal d-block" tabIndex="-1" role="dialog" id="editTutorPrifileModal" style={{background:'rgba(241,243,246,0.6)'}}>
           <div className="modal-dialog modal-dialog-centeredx" role="document">
             <div className="position-absolute" style={{left:'20px',top:'10px', zIndex:'100'}}>
