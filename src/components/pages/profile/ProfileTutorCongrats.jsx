55d561126c8947cf806cb0e74d1bd957a9374ca9
manassorn <manassorn@gmail.com>
2022-03-27 12:37:03 +0700

    rr

--- src/components/pages/profile/ProfileTutorCongrats.jsx
+++ src/components/pages/profile/ProfileTutorCongrats.jsx
@@ -202,7 +202,7 @@ class CheckBoxBadge extends React.Component {
   }
   render() {
     return <div className="form-check form-check-inline border badge badge-pill text py-1 px-2">
-      <input ref={ele => this.checkBox= ele} onClick={this.onClick} className="form-check-input" type="checkbox" id={this.props.label} value={this.props.label}/>
+      <input ref={ele => this.checkBox= ele} onClick={e => this.onClick(e)} className="form-check-input" type="checkbox" id={this.props.label} value={this.props.label}/>
         <label className="form-check-label" htmlFor="inlineCheckbox3">{this.props.label}</label>
     </div>
   }
