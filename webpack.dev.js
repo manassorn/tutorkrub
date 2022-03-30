9db14857574935fc5f195f19055ef24546db1c87
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-03-30 19:58:40 +0700

    asdf

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+      publicPath: '/public'
+    },
     port: 9950,
-    historyApiFallback: { index: 'index.html' },
+    historyApiFallback: { index: '/public/index.html' },
     proxy: {
       '/api': 'http://139.59.216.150:8080',
     },
