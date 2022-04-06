95a20d872deee464eae4014bca014e111ef2a822
manassorn <manassorn@gmail.com>
2022-04-06 14:07:29 +0700

    fd

--- src/components/test.js
+++ src/components/test.js
@@ -1,4 +1,4 @@
 export default function test() {
   
-  return (<div style={{margin:'80px'}}>test<br/>tt</div>)
+  return (<div style={{margin:'80px'}}>test<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff</div>)
 }
