bf003cb76f1aafa9a478caef005ee9303e19602e
manassorn <manassorn@gmail.com>
2022-04-06 14:11:13 +0700

    vv

--- src/components/test.js
+++ src/components/test.js
@@ -1,3 +1,5 @@
+import * as React from 'react'
+
 export default function Test() {
   
   return (<div style={{margin:'80px'}}>test<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff<br/>ttfffff</div>)
