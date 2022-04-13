959237f9e8c4cbe7c1670c7d1daa0111b2249b94
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-04-13 21:13:01 +0700

    a

--- webpack.common.js
+++ webpack.common.js
@@ -19,28 +19,25 @@ const config = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-                // Creates `style` nodes from JS strings
-                "style-loader",
-                // Translates CSS into CommonJS
-                "css-loader",
-                // Compiles Sass to CSS
-                "sass-loader",
-              ],
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          {
+            // Translates CSS into CommonJS
+            loader: "css-loader",
+            options: {
+              // Prefer `dart-sass`
+              url:false,
+            },
+          },
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         // Preprocess your css files
         // you can add additional loaders here (e.g. sass/less etc.)
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(jpg|png|svg)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 25000
-          }
-        }
       }
     ]
   },
