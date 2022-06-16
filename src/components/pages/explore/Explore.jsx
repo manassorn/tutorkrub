88e092a5c01c64b4071db69bc6a763e1409bccb7
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-06-16 11:09:58 +0700

    asdf

--- src/components/pages/explore/Explore.jsx
+++ src/components/pages/explore/Explore.jsx
@@ -55,73 +55,61 @@ class Explore extends React.Component {
     }
 
     render() {
-      return  <div>
-      <div className="container">
-        <div className=" border-bottom pb-0 pt-3">
+      return  (
+        <div>
+          <div className="container">
+            <div className=" border-bottom pb-0 pt-3">
 
-        <div className="row">
-        <div className="col-6 col-sm-3 btn-group-round mb-3">
-          <div className="btn-group d-flex">
-            <button type="button" className="btn btn-white">&nbsp;&nbsp;&nbsp;ชั้นเรียน&nbsp;&nbsp;&nbsp;</button>
-            <div className="dropdown-menu dropdown-menu-right" ariap-labelledby="dropdownSchoolLevel">
-
-            {Constant.schoolLevels.map(label => (
-              <a className="dropdown-item" href="#" key={label}>{label}</a>
-
-            ))}
-            </div>
-            <button id="dropdownSchoolLevel" type="button" className="btn btn-white dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <span className="sr-only">Toggle Dropdown</span>
-            </button>
-        </div>
-        </div>
-
+            <div className="row">
+              <div className="col-6 col-sm-3 btn-group-round mb-3">
+                <select className="form-control form-control-md bg-light" id="exampleFormControlSelect1">
+                  <option>เลือกชั้นเรียน</option>
+                  {Constant.schoolLevels.map(label => (
+                    <option key={label}>{label}</option>
+                  ))}
+                </select>
+              </div>
 
               <div className="col-6 col-sm-3 btn-group-round mb-3">
+                <select className="form-control form-control-md bg-light" id="exampleFormControlSelect1">
+                  <option>เลือกวิชา</option>
+                  {Constant.subjects.map(label => (
+                    <option key={label}>{label}</option>
+                  ))}
+                </select>
+              </div>
 
-                <div className="btn-group d-flex">
+            </div>
 
-                  <button type="button" className="btn btn-white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;วิชา&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-                  <div className="dropdown-menu dropdown-menu-right">
-            {Constant.subjects.map(label => (
-              <a className="dropdown-item" href="#" key={label}>{label}</a>
+
+           </div>
+
+            <div className="my-4">
+            {/*  <h1>คณิตศาสตร์</h1>*/}
+            </div>
+            <div id="course-cards" className="row">
+
+            {this.state.tutors.map(tutor => (
+              <Card4 key={tutor.id} tutor={tutor} isFavorite={this.isFavorite(tutor.id)} toggleFavorite={this.toggleFavorite}/>
 
             ))}
-                  </div>
-                  <button type="button" className="btn btn-white dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <span className="sr-only">Toggle Dropdown</span>
-                  </button>
-                </div>
-              </div>
-        </div>
-
-
-       </div>
-
-        <div className="my-4">
-        {/*  <h1>คณิตศาสตร์</h1>*/}
-        </div>
-        <div id="course-cards" className="row">
-
-        {this.state.tutors.map(tutor => (
-          <Card4 key={tutor.id} tutor={tutor} isFavorite={this.isFavorite(tutor.id)} toggleFavorite={this.toggleFavorite}/>
-
-        ))}
 
 
 
+
+            </div>
+
+
+
+            <div className="row">
+             <div className="col-sm-12 col-md-6 offset-md-3">
+              {/*<button className="btn btn-block btn-outline-primary">next</button>*/}
+             </div>
+            </div>
+           </div>
 
         </div>
-
-
-
-        <div className="row">
-         <div className="col-sm-12 col-md-6 offset-md-3">
-          {/*<button className="btn btn-block btn-outline-primary">next</button>*/}
-         </div>
-        </div>
-       </div>
-
-    </div>
-
+      )
     }
 }
 export default Explore
