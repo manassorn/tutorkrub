53b225d291a1bbb747662c901007c4b61efdcbac
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-06-23 09:51:12 +0700

    asdf

--- src/components/pages/explore/Card4.jsx
+++ src/components/pages/explore/Card4.jsx
@@ -28,38 +28,43 @@ class Card4 extends React.Component {
 
     return (
       <div className="col-md-6 mb-3">
-        <Link to={`/tutor/${tutor.id}`}>
-          <div className="border radius-10 p-3 bg-white">
-            <div className="d-flex align-items-centerx">
-              <div>
-                <img className="mr-3 rounded-circle mb-3" src={tutor.avatarUrl} alt="" width="80"/>
-              </div>
-              <div className="flex-grow-1">
-                <h5 className="font-weight-bold mt-2 mb-1">{tutor.krubId}</h5>
+        <div className="card">
+          <div className="card-body">
 
+            <div className="media">
+              <div className="text-center">
+                <img className="rounded-circle mb-2 p-1 border" src={tutor.avatarUrl} alt="" width="80"/>
+                <br/>
                 <p className="text-secondary mb-3">ติวเตอร์</p>
+              </div>
+              <div className="media-body pl-3">
+                <h5 className="font-weight-bold mt-2 mb-3">{tutor.krubId}</h5>
+
+                <div>
+                  <i className="lni lni-graduation bx-xs mr-1 text-primary" />
+                  <span>{tutor.tutorSubjects && tutor.tutorSubjects.join(', ')}</span>
+                </div>
+                <div>
+                  <i className="lni lni-flag-alt bx-xs mr-1 text-primary" />
+                  <span>{tutor.tutorLevels && tutor.tutorLevels.join(', ') }</span>
+
+                </div>
+                <div>
+                  <i className="lni lni-coin bx-xs mr-1 text-primary" />
+                  <span>฿{tutor.price || tutor.tutorPrice} / ชั่วโมง</span>
+
+                </div>
+                <div className="mt-3">
+                  <Link to={`/tutor/${tutor.id}`} className="stretched-link">ดูรายละเอียด</Link>
+                </div>
 
               </div>
               <a href="#" style={{width:'26px'}} className={`heart ${this.state.isFavorite?'active':''}`} onClick={e => this.toggleFavorite(tutor.id)}>&nbsp;
               </a>
             </div>
-            <div>
-              <i className="lni lni-graduation bx-xs mr-1 text-primary" />
-              <span>{tutor.tutorSubjects && tutor.tutorSubjects.join(', ')}</span>
-            </div>
-            <div>
-              <i className="lni lni-flag-alt bx-xs mr-1 text-primary" />
-              <span>{tutor.tutorLevels && tutor.tutorLevels.join(', ') }</span>
-
-            </div>
-            <div>
-              <i className="lni lni-coin bx-xs mr-1 text-primary" />
-              <span>฿{tutor.price || tutor.tutorPrice} / ชั่วโมง</span>
-
-            </div>
 
           </div>
-        </Link>
+        </div>
       </div>
     )
   }
--- src/components/pages/tutor/Tutor.js
+++ src/components/pages/tutor/Tutor.js
@@ -4,15 +4,11 @@ import Api from '../../../Api'
 
 function Tutor() {
   let { tutorId } = useParams();
-  console.log(tutorId)
   const [tutor, setTutor] = useState(null)
 
   useEffect(() => {
-    console.log(tutorId)
-    // const a = location.href.split('/')
-    // const id = a[a.length-1]
     Api.get(`/search?tutorid=${tutorId}`).then(response => {
-      setTutor(response.data.data)
+      setTutor(response.data.data[0])
     }).catch(error => {
       console.error(error)
     })
@@ -20,14 +16,57 @@ function Tutor() {
 
   return (
     <div className="container pt-5">
-      <div className="col-12 col-lg-4 col-xl-3">
-        <div className="card">
-          <img src="/public/assets/images/gallery/02.jpg" className="card-img-top" alt="..."/>
+      <div className="row">
+        <div className="col-12 col-lg-8 col-xl-8">
+          <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p> <a href="#" className="btn btn-primary">Go somewhere</a>
+              <div className="media">
+                <div className="text-center">
+                  <img src={tutor?tutor.avatarUrl:' '} style={{maxWidth:'100px'}} className="m3 rounded-circle p-1 border" alt="..."/>
+                  <br/>
+                  <span className="badge badge-pill badge-success mt-3">ใหม่</span>
+                </div>
+                <div className="media-body pl-3">
+                  <h5 className="card-title">{tutor?tutor.krubId:' '} </h5>
+                  <p className="card-text">
+                    <span className="text-muted pr-3">ชั้นเรียน</span>
+                    {tutor?tutor.tutorLevels: ' '}
+                    <br/>
+                    <span className="text-muted pr-3">วิชา</span>
+                    {tutor?tutor.tutorSubjects: ' '}
+                    <br/>
+                    <span className="text-muted pr-3">ราคา</span>
+                    {tutor?tutor.tutorPrice: ' '} บาทต่อชั่วโมง
+                  </p>
+                  <p className="card-text">
+                  </p>
+                  <a href="#" className="btn btn-primary">นัดหมายเวลา</a>
+                </div>
+              </div>
             </div>
+            <div className="border-top d-flex">
+              <div className="p-2 w-100 text-center border-right" style={{color:'#DADADA'}}>&#9733;&nbsp;&#9733;&nbsp;&#9733;&nbsp;&#9733;&nbsp;&#9733;</div>
+              <div className="p-2 w-100 text-center border-right">0 รีวิว</div>
+              <div className="p-2 w-100 text-center">0 ชั่วโมง</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-4 col-xl-4">
+          <div className="card">
+            <div className="card-body">
+              a
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-8 col-xl-8">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">แนะนำตัว </h5>
+              <p className="card-text">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
