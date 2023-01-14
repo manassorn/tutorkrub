7fcd2e2a8caf247202f817b3c79e7b7dfa2b6a80
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2023-01-14 22:25:16 +0700

    admin tutor add

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 module.exports = merge(common, {
   mode: 'production',
